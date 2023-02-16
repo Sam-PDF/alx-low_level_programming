5f10eb85ad97cc001603d66032dfe656e08195bd
Sam-PDF <samolofinji@gmail.com>
2023-02-16 15:50:52 -0800

    c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-1 #include <stdio.h>
-2
-3  /**
-4  *main - Entry point
-5  Description: prints "/"Programming is like building a multilingual puzzle"
-6  Return: 0
-7  /
-8
-9  int main(void)
-10 {
-11	puts("/"Programming is like building a multilingual puzzle");
-12	return (0);
-13 }
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   * Description: prints "\"Programming is like building a multilingual puzzle"
+ *    * Return: 0
+ *     */
+
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
